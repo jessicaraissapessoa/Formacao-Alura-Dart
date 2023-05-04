9febd112301043972840e62d0359cc9127467f91
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-05-04 14:31:41 -0300

    04/05/23

--- bin/sintaxe_colecoes_dinamismo.dart
+++ bin/sintaxe_colecoes_dinamismo.dart
@@ -1,3 +1,33 @@
 void main() {
 
+  escolherMeioTransporte(Transporte.carro);
+
+}
+
+
+void escolherMeioTransporte(Transporte locomocao) {
+
+  switch(locomocao){
+    case Transporte.carro:
+      print("vou de CARRO para a aventura!");
+      break;
+    case Transporte.bike:
+      print("vou de BIKE para a aventura!");
+      break;
+    case Transporte.onibus:
+      print("vou de ÔNIBUS para a aventura!");
+      break;
+    default:
+      print("Estou indo para a aventura. Isso é o que importa");
+      break;
+  }
+
+}
+
+enum Transporte {
+  //cada elemento no enum, é um elemento para comparação
+  carro,
+  bike,
+  onibus,
+  skate
 }
