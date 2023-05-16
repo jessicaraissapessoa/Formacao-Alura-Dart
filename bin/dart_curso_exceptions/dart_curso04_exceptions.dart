bb7c38336ba28331f860e2005a3d22065694ec48
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-05-16 00:22:38 -0300

    16/05/23

--- bin/dart_curso_exceptions/dart_curso04_exceptions.dart
+++ bin/dart_curso_exceptions/dart_curso04_exceptions.dart
@@ -8,7 +8,14 @@ void main() {
 
 void functionOne() {
   print('Started F01');
-  functionTwo();
+
+  //tratando a exceção ao redor da functionTwo:
+  try {
+    functionTwo();
+  } on FormatException {
+    print('Foi capturada dentro da FunctionOne');
+  }
+
   print('Finished F01');
 }
 
@@ -16,6 +23,17 @@ void functionTwo() {
   print('Started F02');
   for(int i = 1; i <= 5; i++) {
     print(i);
+
+    /*tratando exceção na linha:
+    try{
+      double amount = double.parse("not a number");
+    } on FormatException {
+      print('A conversão não pôde ser feita');
+    }
+     */
+
+    double amount = double.parse("not a number");
+
   }
   print('Finished F02');
 }
